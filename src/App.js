3ede835697984e95c7a31420a58aaa991070cd8c
Laxmiba7 <oluxmee@gmail.com>
2022-03-25 23:42:52 +0545

    Add home,quote,navbar component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Navbar />
         <Calculator />
       </div>
     );
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="menu">
+
+      <div>
+        <h1>Math Magician</h1>
+      </div>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Calculator</a></li>
+        <li><a href="#">Quotes</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
